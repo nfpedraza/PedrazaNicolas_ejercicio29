c4ac2f3b8adb9f80a2bf2758c18f1d676ad3f8c1
nfpedraza <nifepegi@gmail.com>
2019-11-19 16:52:28 +0000

    arreglo

--- Clase_29.cpp
+++ Clase_29.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
+
 //Codigo extraido y modificado de : https://www.dreamincode.net/forums/topic/125770-simulating-diffusion-equation/
 
 using namespace std;
 
 int main ()
 {
-    int x_n=30;
-    int t_n=30;
-    double sigma = 0.2;
+    const int x_n=30;
+    const int t_n=10;
+    const double sigma = 1.0;
+    const double D= 1.0;
     double dx=1.0/x_n;
-    double dt= sigma * pow(dx,2.0);
+    double dt= sigma * pow(dx,2.0)/2*D;
     double C[x_n][t_n];
     double x=0.0;
     double mu=0.5;
@@ -34,7 +36,7 @@ int main ()
         for(int i=1; i<x_n-1; i++){
             x=i*dx;
 			
-            C[i][j+1] = C[i][j] +(dt/pow(dx,2))*(C[i+1][j] - 2*C[i][j] + C[i-1][j]);
+            C[i][j+1] = C[i][j] +(D*dt/pow(dx,2))*(C[i+1][j] - 2*C[i][j] + C[i-1][j]); //aqui se define la ecuacion de difusion del libro
         }
 
         C[0][j+1]=0.0;                             //boundary condition i)      
@@ -57,5 +59,5 @@ int main ()
 
     fout.close();
 
-    return 0; // Need to return an int here.                                    
+    return 0;                                    
 }
